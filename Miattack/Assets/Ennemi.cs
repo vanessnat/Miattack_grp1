0be2866e0330a4f15143ef8573c200247faf79dc
vanessnat <natachatwyffels@gmail.com>
2023-05-03 15:44:09 +0200

    ennemi disparition

--- Miattack/Assets/Ennemi.cs
+++ Miattack/Assets/Ennemi.cs
@@ -2,9 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Ennemi : MonoBehaviour
 {
+    public GameObject deathEffect;
     public float health = 4f;
+    public static int EnnemisAlive = 0;
+
+    void Start ()
+    {
+        EnnemisAlive++;
+    }
 
     void OnCollisionEnter2D (Collision2D colInfo)
     {
@@ -17,6 +25,14 @@ public class Ennemi : MonoBehaviour
     
     void Die ()
     {
+        GameObject particle = Instantiate(deathEffect, transform.position, Quaternion.identity);
+
+        particle.SetActive(true);
+
+        EnnemisAlive--;
+        if (EnnemisAlive<=0)
+        
         Destroy(gameObject);
+    
     }
 }
