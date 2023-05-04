565a98ce259d5f48f4cc7b7623b40cc54f801b24
chloaye <chloe.faye@mmibordeaux.com>
2023-05-04 21:13:52 +0200

    Menu Pause

---
+++ Miattack/Assets/Scene 3/Pause/PauseObject.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Translate(Vector3.down * Time.deltaTime);
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+}
