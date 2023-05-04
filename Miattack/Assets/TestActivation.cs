16d747a9c7aa81764d2b02ac9a64b25721e77529
chloaye <chloe.faye@mmibordeaux.com>
2023-05-04 10:36:59 +0200

    Black and White

---
+++ Miattack/Assets/TestActivation.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.PostProcessing;
+
+
+public class TestActivation : MonoBehaviour
+{
+     public PostProcessVolume volume;
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  volume.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
